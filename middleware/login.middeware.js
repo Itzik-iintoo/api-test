9d594db7ada80ce68fe7a5ee9c0244ff81eef875
Yitz <chalfari@gmail.com>
2022-03-14 15:26:43 +0200

    add locutus in encrypt middleware

--- middleware/login.middeware.js
+++ middleware/login.middeware.js
@@ -1,9 +1,9 @@
 const sha1 = require("sha1");
 const locutus = require("locutus/php/strings/sha1");
-console.log(locutus("123456"), sha1("123456"));
+// console.log(locutus("123456"), sha1("123456"));
 const encryptPassword = (req, res, next) => {
   console.log(req.body);
-  req.body.password = sha1(req.body.password);
+  req.body.password = locutus(req.body.password);
   next();
 };
 module.exports = { encryptPassword };
