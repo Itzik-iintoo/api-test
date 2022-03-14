725cff99edf3c8700aa045ecbbd2facffc463857
Yitz <chalfari@gmail.com>
2022-03-14 15:26:19 +0200

    add locutus since sha1 returned unexpected results

--- middleware/login.middeware.js
+++ middleware/login.middeware.js
@@ -1,5 +1,6 @@
 const sha1 = require("sha1");
-
+const locutus = require("locutus/php/strings/sha1");
+console.log(locutus("123456"), sha1("123456"));
 const encryptPassword = (req, res, next) => {
   console.log(req.body);
   req.body.password = sha1(req.body.password);
